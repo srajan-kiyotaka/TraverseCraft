c636ce862f45a7a7a62a32aa77e0d74705ded561
srajan-kiyotaka <srajanstark.ash@gmail.com>
2024-01-07 22:53:51 +0530

    made the path color and block color variables.

--- traverseCraft/world.py
+++ traverseCraft/world.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 class CreateGridWorld:
     """
     """
-    def __init__(self, worldName:str, rows:int, cols:int, cellSize:int=10, cellColor:str="white", cellPadding:int=2, borderWidth:int=1):
+    def __init__(self, worldName:str, rows:int, cols:int, cellSize:int=10, pathColor:str="green", blockColor:str="red", cellPadding:int=2, borderWidth:int=1):
         # ~~~~~ World Attributes ~~~~~ #
         self._worldName = worldName
         self._rows = rows
@@ -13,8 +13,9 @@ class CreateGridWorld:
         self._blockCells = None
         # ~~~~~ Cell Attributes ~~~~~ #
         self._cellSize = cellSize
-        self._cellColor = cellColor
+        self._pathColor = pathColor
         self._cellPadding = cellPadding
+        self._blockColor = blockColor
         self._borderWidth = borderWidth
         self.cells = None
         # ~~~~~ Cell Styles ~~~~~ #
@@ -26,7 +27,7 @@ class CreateGridWorld:
         self._root.title(self._worldName)
         self._root.geometry(f"{self._rows * (self._cellSize + 2*self._cellPadding)}x{self._cols * (self._cellSize + 2*self._cellPadding)}")
         self._world = [[0 for i in range(self._cols)] for j in range(self._rows)]
-        self._cells = [[None for j in range(self._cols)] for i in range(self._rows)]
+        self._cells = [[Frame(self._root, width=self._cellSize, height=self._cellSize, bg=self._pathColor, borderwidth=self._borderWidth) for j in range(self._cols)] for i in range(self._rows)]
 
     def __str__(self):
         return f"World Name: {self._worldName}\nRows: {self._rows}\nCols: {self._cols}\nWindow Size: {self._rows * (self._cellSize + self._cellPadding)}x{self._cols * (self._cellSize + self._cellPadding)}"
@@ -34,10 +35,10 @@ class CreateGridWorld:
     def constructWorld(self):
         """
         """
-        for i in range(self._rows):
-            for j in range(self._cols):
-                if(self._cells[i][j] is None):
-                    self._cells[i][j] = Frame(self._root, width=self._cellSize, height=self._cellSize, bg="green", borderwidth=self._borderWidth)
+        # for i in range(self._rows):
+        #     for j in range(self._cols):
+        #         if(self._cells[i][j] is None):
+        #             self._cells[i][j] = Frame(self._root, width=self._cellSize, height=self._cellSize, bg=self._pathColor, borderwidth=self._borderWidth)
 
         # Add cells to the grid using the Grid geometry manager
         for i in range(self._rows):
@@ -49,7 +50,7 @@ class CreateGridWorld:
         """
         self._blockCells = blockCells
         for i in range(len(self._blockCells)):
-            self._cells[self._blockCells[i][0]][self._blockCells[i][1]] = Frame(self._root, width=self._cellSize, height=self._cellSize, bg="red", borderwidth=self._borderWidth)
+            self._cells[self._blockCells[i][0]][self._blockCells[i][1]] = Frame(self._root, width=self._cellSize, height=self._cellSize, bg=self._blockColor, borderwidth=self._borderWidth)
             self._world[self._blockCells[i][0]][self._blockCells[i][1]] = -1
 
     def showWorld(self):
@@ -65,7 +66,7 @@ class CreateGridWorld:
 # class CreateGraphWorld:
 
 
-world = CreateGridWorld("Grid Test Run", rows = 10 , cols = 10, cellSize = 50, cellColor = "gray")
+world = CreateGridWorld("Grid Test Run", rows = 20, cols = 20, cellSize = 40)
 world.blockPath([[0,0],[1,1],[4,2],[7,3],[8,4],[2,5],[9,6],[2,7]])
 world.constructWorld()
 world.printWorld()
