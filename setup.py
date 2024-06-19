33db45be4c0b9471450d2ce60c709ceae47039ee
srajan-kiyotaka <srajanstark.ash@gmail.com>
2024-06-19 23:46:38 +0530

    change the version to beya 0.8.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='TraverseCraft',
-    version='0.8.0',
+    version='0.8.5',
     author='Srajan Chourasia, Varun Patrikar',
     author_email='srajanstark.ash@gmail.com, patrikarvarun@gmail.com',
     maintainer='Srajan Chourasia, Varun Patrikar',
